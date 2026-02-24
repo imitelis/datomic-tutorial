cea6c7e9c938e01dea7e7e7c83a2bc3412e27866
imitelis <dsaavedra@unal.edu.co>
2026-02-23 19:42:18 -0500

    add: predicate queries

---
+++ src/datomic_tutorial/queries/predicates.clj
@@ -0,0 +1,22 @@
+(ns datomic-tutorial.queries.predicates
+  (:require [datomic.api :as d]
+            [datomic-tutorial.core :refer [conn]]))
+
+;; Predicates
+
+;; Find all movies released before 1984
+(d/q '[:find ?title
+       :where
+       [?m :movie/title ?title]
+       [?m :movie/year ?year]
+       [(< ?year 1984)]]
+     (d/db conn))
+
+;; Java method as a predicate function:
+(d/q '[:find ?title
+       :where
+       [?m :movie/title ?title]
+       [?m :movie/year ?year]
+       [(< ?year 1984)]
+       [(java.lang.String/startsWith ?title "The")]]
+     (d/db conn))
