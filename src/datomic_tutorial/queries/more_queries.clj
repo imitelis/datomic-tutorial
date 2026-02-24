ceb3e9a88bf3ff5c676f2448875a4215c10b0eb9
imitelis <dsaavedra@unal.edu.co>
2026-02-23 19:21:31 -0500

    add: more queries

---
+++ src/datomic_tutorial/queries/more_queries.clj
@@ -0,0 +1,36 @@
+(ns datomic-tutorial.queries.more-queries 
+  (:require [datomic-tutorial.core :refer [conn]]))
+  
+(require '[datomic.api :as d])
+
+;; A datom is a 4-tuple [eid attr val tx]
+
+
+;; Attributes
+
+;; We know for certain that :person/name is one such attribute
+(d/q '[:find ?attr
+       :where
+       [?p :person/name]
+       [?p ?attr]]
+     (d/db conn))
+
+;; Find all attributes that are associated with person entities in our database
+(d/q '[:find ?attr
+       :where
+       [?p :person/name]
+       [?p ?attr]]
+     (d/db conn))
+
+;; This is because attributes are also entities in our database!
+
+
+;; Transactions
+
+;; It's also possible to run queries to find information about transactions
+;; The transaction entity is the fourth element in the datom vector
+(d/q '[:find ?timestamp 
+       :where
+       [?p :person/name "James Cameron" ?tx]
+       [?tx :db/txInstant ?timestamp]] 
+     (d/db conn))
---
+++ src/datomic_tutorial/queries/parametrized_queries.clj
@@ -0,0 +1,78 @@
+(ns datomic-tutorial.queries.parametrized-queries
+  (:require [datomic-tutorial.core :refer [conn]]))
+  
+(require '[datomic.api :as d])
+
+
+;; Original query
+(d/q '[:find ?title 
+       :where 
+       [?p :person/name "Sylvester Stallone"] 
+       [?m :movie/cast ?p] 
+       [?m :movie/title ?title]]
+     (d/db conn))
+
+;; Parametrized query
+(def query '[:find ?title
+             :in $ ?name
+             :where
+             [?p :person/name ?name]
+             [?m :movie/cast ?p]
+             [?m :movie/title ?title]])
+
+;; The above query is executed like
+(d/q query (d/db conn) "Sylvester Stallone")
+
+;; Each data pattern is actually a 5-tuple (EAV data model extended)
+;; [<database> <entity-id> <attribute> <value> <transaction-id>]
+;; It's just that the database part is implicit
+
+
+;; Tuple query
+(d/q '[:find ?title
+       :in $ [?director ?actor]
+       :where
+       [?d :person/name ?director]
+       [?a :person/name ?actor]
+       [?m :movie/director ?d]
+       [?m :movie/cast ?a]
+       [?m :movie/title ?title]]
+     (d/db conn))
+
+;; Instead of two distinct inputs
+;; :in $ ?director ?actor
+
+
+;; Collection query
+(d/q '[:find ?title
+       :in $ [?director ...]
+       :where
+       [?p :person/name ?director]
+       [?m :movie/director ?p]
+       [?m :movie/title ?title]]
+     (d/db conn))
+
+;; To find all movies directed by either one director or the other
+;; ?director
+
+
+;; Relational query
+
+;; Consider a relation with tuples [movie-title box-office-earnings]:
+;; [
+;; ...
+;; ["Die Hard" 140700000]
+;; ["Alien" 104931801]
+;; ["Lethal Weapon" 120207127]
+;; ["Commando" 57491000]
+;; ...
+;; ]
+
+;; Find box office earnings for a particular director 
+(d/q '[:find ?title ?box-office
+       :in $ ?director [[?title ?box-office]]
+       :where
+       [?p :person/name ?director]
+       [?m :movie/director ?p]
+       [?m :movie/title ?title]]
+     (d/db conn))
